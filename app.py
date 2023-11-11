386b540b485ee118ff15e7cdba5ae12aaa2b4e18
ali <ali.maharramov@gmail.com>
2023-11-11 15:39:59 +0200

    Jump count working

--- app.py
+++ app.py
@@ -1,15 +1,57 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, send_file
+import numpy as np
+import scipy.signal as sps
 app = Flask(__name__, template_folder='templates')
+
+# Initialize a dictionary to store accelerometer data for each client
+client_data_dict = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/get_result', methods=['GET'])
+def get_result():
+    global client_data_dict
+    '''function receive an np.array as an input and returns count,
+    requires import numpy as np, import scipy.signal as sps'''
+    count={}
+    # cleaning the array from Nones
+    for key in client_data_dict:
+        accelerationY=client_data_dict[key]
+        arr = accelerationY[accelerationY != np.array(None)]
+    
+    # min-max normalization
+        xmax = arr.max()
+        xmin = arr.min()
+        arr = (arr - xmin) / (xmax - xmin)
+    
+    # counting the peaks (! basic consideration: refreshing rate = 100 Hertz, thus distance=30 is 0.3 seconds)
+        count[key] = len(sps.find_peaks(arr, height=0.6, distance=30)[0])
+        client_data_dict[key]=np.array([])
+    client_data_dict = {}
+    
+    return count
+
+
+
 @app.route('/accelerometer', methods=['POST'])
 def accelerometer():
     data = request.json
-    print("Accelerometer data received:", data)
+    client_ip = request.remote_addr
+    # print("Accelerometer data received:", data['accelerationIncludingGravity']['y'], client_ip)
+
+    # Check if the client already has an array, if not, initialize one
+    if client_ip not in client_data_dict:
+        client_data_dict[client_ip] = np.array([])
+
+    # Add the received data to the client's array
+    client_data_dict[client_ip] = np.append(client_data_dict[client_ip], data['acceleration']['y'])
+
+    filename = f'save_{client_ip}.npy'
+    np.save(filename, client_data_dict[client_ip])
+
     return 'OK'
 
 if __name__ == '__main__':
---
+++ reader.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arra=np.load('save_10.0.15.61.npy', allow_pickle=True)
+
+for element in arra:
+    print(element)
+
