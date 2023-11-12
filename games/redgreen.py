2a120efea0fc7aa306cd85e2204b3fb35c2c25e3
ali-609 <55524661+ali-609@users.noreply.github.com>
2023-11-12 09:08:28 +0200

    Update redgreen.py

--- games/redgreen.py
+++ games/redgreen.py
@@ -1,12 +1,12 @@
-# Import necessary modules from Flask for web application development
+# Import necessary modules application development
 from flask import Blueprint, render_template, request, send_file
 import numpy as np
 import scipy.signal as sps
 
-# Create a Blueprint instance for the 'redgreen' game module
+# Create a Blueprint instance for 'Red light Green light' game 
 red_blueprint = Blueprint('redgreen', __name__, template_folder='custom_templates')
 
-# Define a route that renders the main page for the 'redgreen' game module
+# Define a route that renders the main page for the 'Red light Green light' game
 @red_blueprint.route('/')
 def redgreen():
     return render_template('redgreen.html')
