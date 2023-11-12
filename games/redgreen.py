b2c0f3d914a5d2ec9bd380ca9c25ca163be72e42
ali-609 <55524661+ali-609@users.noreply.github.com>
2023-11-12 09:07:34 +0200

    Comments

--- games/redgreen.py
+++ games/redgreen.py
@@ -1,11 +1,12 @@
-# File: games/jump.py
+# Import necessary modules from Flask for web application development
 from flask import Blueprint, render_template, request, send_file
 import numpy as np
 import scipy.signal as sps
 
+# Create a Blueprint instance for the 'redgreen' game module
 red_blueprint = Blueprint('redgreen', __name__, template_folder='custom_templates')
 
-
+# Define a route that renders the main page for the 'redgreen' game module
 @red_blueprint.route('/')
 def redgreen():
     return render_template('redgreen.html')
